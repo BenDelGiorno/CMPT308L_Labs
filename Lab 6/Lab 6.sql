353c6624cef1eb5cfa0ec27cf132aa65e91a3273
Ben DelGiorno <benjamin.delgiorno1@marist.edu>
2014-02-27 22:16:58 -0500

    Lab Assignment 6

--- Lab 6/Lab 6.sql
+++ Lab 6/Lab 6.sql
@@ -3,7 +3,7 @@
 -- CMPT308L
 
 
--- Q1-Get	the	name	and	city	of	customers	who	live	in	a	city	where	the	most	number	of products	are	made.	
+-- Q1-Get the name and city of customers who live in a city where the most number of products are made.	
 select c.name, c.city
 from customers c
 where c.city in (select p.city
@@ -15,7 +15,7 @@ where c.city in (select p.city
 
 
 
--- Q2-Get	the	name	and	city	of	customers	who	live	in	any	city	where	the	most	number	of products	are	made.	
+-- Q2-Get the name and city of customers who live in any city where the	most number of products are made.	
 select c.name, c.city
 from customers c
 where c.city in (select p1.city
@@ -30,7 +30,7 @@ where c.city in (select p1.city
                   );
 
 
--- Q3-List	the	products	whose	priceUSD	is	above	the	average	priceUSD.	
+-- Q3-List the products whose priceUSD is above the average priceUSD.	
 select name, avg(priceUSD)
 from products
 group by name, priceUSD
@@ -38,22 +38,22 @@ having priceUSD > (select avg(priceUSD)
                     from products);
                     
                     
--- Q4-Show	the	customer	name,	pid	ordered,	and	the	dollars	for	all	customer	orders,	sorted by	dollars	from	high	to	low.	
+-- Q4-Show the customer name, pid ordered, and the dollars for all customer orders, sorted by dollars from high to low.	
 select c.name, o.pid, o.dollars
 from customers c inner join orders o on c.cid = o.cid
 order by o.pid asc,
           o.dollars desc;
 
 
--- Q5-Show	all	customer	names	(in	order)	and	their	total	ordered,	and	nothing	more.	Use coalesce	to	avoid	showing	NULLs.	
+-- Q5-Show all customer names (in order) and their total ordered, and nothing more. Use coalesce to avoid showing NULLs.	
 select c.name, c.city, coalesce(sum(o.qty), 0) as "Total Ordered"
 from customers c left outer join orders o on c.cid = o.cid
 group by c.name, c.city
 order by c.name asc;
 
--- Q6-Show	the	names	of	all	customers	who	bought	products	from	agents	based	in	New	
--- York	along	with	the	names	of	the	products	they	ordered,	and	the	names	of	the	agents	
--- who	sold	it	to	them.	
+-- Q6-Show the names of all customers who bought products from agents based in New
+-- York along with the names of the products they ordered, and the names of the agents	
+-- who sold it to	them.	
 select c.name, p.name, a.name
 from orders o inner join customers c on o.cid = c.cid
                inner join products p on o.pid = p.pid
@@ -61,9 +61,9 @@ from orders o inner join customers c on o.cid = c.cid
 where a.city = 'New York';
 
 
--- Q7-Write	a	query	to	check	the	accuracy	of	the	dollars	column	in	the	Orders	table.	This	
--- means	calculating	Orders.dollars	from	other	data	in	other	tables	and	then	comparing	
--- those	values	to	the	values	in	Orders.dollars
+-- Q7-Write a query to check the accuracy of the dollars column in the Orders table. This	
+-- means calculating Orders.dollars from other data in other tables and then comparing	
+-- those values to the values in Orders.dollars
 select *
 from orders o inner join customers c on o.cid = c.cid
                inner join products p on o.pid = p.pid
